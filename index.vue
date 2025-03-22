67435bc3c85083ef19f0e4e051c374577e77b6dc
bothrachahat <bothrachahat@gmail.com>
2025-03-22 18:01:23 +0530

    Add custom homepage

---
+++ index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Welcome to My Nuxt App!</h1>
+    <p>This is my custom homepage!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
